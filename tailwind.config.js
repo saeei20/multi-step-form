61f22d38376ca60541e7ccf3541e3ef48d8a1b15
saeei20 <144978732+saeei20@users.noreply.github.com>
2024-04-10 19:50:09 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    mode: 'jit',
+    purge: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+      extend: {},
+    },
+    variants: {
+      extend: {},
+    },
+    plugins: [],
+  }
+  
